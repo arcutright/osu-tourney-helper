6e008b1d589a30e63df59df6250340daf58f3a40
Aaron Cutright <aaron.cutwrong@gmail.com>
2023-03-01 22:17:07 -0500

    Add lock to console writes, add disposable wrapper for writing blocks of text

--- console.py
+++ console.py
@@ -38,59 +38,41 @@ class Console:
     is re-written. This makes it feel like the conosle prompt "floats" at 
     the bottom of the console.
     """
-    __LOCK = Lock()
-    __last_source = '' # blank = not the console, 'c' = the console
-    __console_stdout = []
-    __console_stderr = []
+    _LOCK = Lock()
+    _last_source = '' # blank = not the console, 'c' = the console
+    _console_stdout = []
+    _console_stderr = []
 
     @staticmethod
     def flush():
-        with Console.__LOCK:
+        with Console._LOCK:
             sys.stdout.flush()
 
     @staticmethod
     def flush_stderr():
-        with Console.__LOCK:
+        with Console._LOCK:
             sys.stderr.flush()
 
-    @staticmethod
-    def __count_console_chars(line: str):
-        return sum(-1 if ch == '\b' else (0 if ch in ('\n', '\r') else 1) for ch in line)
 
     @staticmethod
-    def __writeln(target_name: str, line: str, next_source: str = ''):
-        with Console.__LOCK:
-            target = sys.stderr if target_name == 'stderr' else sys.stdout
-            last_source = Console.__last_source
-            if last_source == 'c' and next_source == 'c':
-                target.write(line)
-                if not line.endswith('\n'):
-                    target.write('\n')
-            else:
+    def __writeln(target_name: str, line: str, source: str = ''):
+        with Console.LockedWriter(target_name, source) as writer:
+            writer.writeln(line)
+    
+    @staticmethod
+    def __console_write(target_name: str, text: str):
+        if text.endswith('\n'):
+            Console.__writeln(target_name, text, 'c')
+        else:
+            with Console._LOCK:
                 if target_name == 'stderr':
-                    prev_output = ''.join(Console.__console_stderr)
+                    sys.stderr.write(text)
+                    Console._console_stderr.append(text)
                 else:
-                    prev_output = ''.join(Console.__console_stdout)
-                if len(prev_output) > 0:
-                    n = Console.__count_console_chars(prev_output)
-                    if n - len(line) > 0:
-                        target.write(' '*n)
-                    target.write('\r')
-                    target.write(line)
-                    if not line.endswith('\n'):
-                        target.write('\n')
-                    target.write(prev_output)
-                else:
-                    target.write(line)
-                    if not line.endswith('\n'):
-                        target.write('\n')
-            if next_source == 'c':
-                if target_name == 'stderr':
-                    Console.__console_stderr.clear()
-                else:
-                    Console.__console_stdout.clear()
-            Console.__last_source = next_source
-
+                    sys.stdout.write(text)
+                    Console._console_stdout.append(text)
+                Console._last_source = 'c'
+    
     @staticmethod
     def writeln(text: str):
         Console.__writeln('stdout', text)
@@ -108,23 +90,82 @@ class Console:
         Console.__writeln(sys.stderr, text, 'c')
 
     @staticmethod
-    def __console_write(target_name: str, text: str):
-        if text.endswith('\n'):
-            Console.__writeln(target_name, text, 'c')
-        else:
-            with Console.__LOCK:
-                if target_name == 'stderr':
-                    sys.stderr.write(text)
-                    Console.__console_stderr.append(text)
-                else:
-                    sys.stdout.write(text)
-                    Console.__console_stdout.append(text)
-                Console.__last_source = 'c'
-
-    @staticmethod
     def _write(text: str):
         Console.__console_write('stdout', text)
     
     @staticmethod
     def _write_stderr(text: str):
         Console.__console_write('stderr', text)
+
+
+    class LockedWriter(object):
+        """ Disposable wrapper for grabbing the console lock and doing a block of writes.
+        This is to avoid constantly rewriting the 'console prompt' after each write,
+        which would happen if you use Console.writeln()
+
+        usage:
+          with LockedWriter() as writer:
+              writer.writeln('line 1');
+               writer.writeln('line 2');
+        """
+        def __init__(self, target_name='stdout', source=''):
+            """
+            arguments:
+              target_name: 'stderr' -> sys.stderr, else -> sys.stdout
+              source: 'c' -> 'the console prompt', either writing the conosle prompt itself or spitting out user keystrokes
+                       else -> not the console prompt. after this writer is finished, it will ensure the block of writes
+                               ends with a \\n and then will write out the console prompt (+ any in-progress user input)
+            """
+            self.target_name = target_name
+            self.target = sys.stderr if target_name == 'stderr' else sys.stdout
+            self.source = source
+            self.prev_console_output = ''
+            self.last_source = ''
+            self.last_text = ''
+            self.wrote_newline = False
+        
+        @staticmethod
+        def __count_console_chars(line: str):
+            return sum(-1 if ch == '\b' else (0 if ch in ('\n', '\r') else 1) for ch in line)
+
+        def __enter__(self):
+            Console._LOCK.acquire()
+            self.last_source = Console._last_source
+            if self.last_source == self.source == 'c':
+                return self
+            if self.target_name == 'stderr':
+                self.prev_console_output = ''.join(Console._console_stderr)
+            else:
+                self.prev_console_output = ''.join(Console._console_stdout)
+            n = len(self.prev_console_output)
+            if n > 0:
+                # erase the conosle output by overwriting it with space chars
+                self.target.write(' '*n)
+                self.target.write('\r')
+            return self
+
+        def writeln(self, text: str):
+            """ write a line of text (and adds \\n if text does not end with \\n) """
+            if not text.rstrip(' \t\b').endswith('\n'):
+                text += '\n'
+            self.target.write(text)
+            self.last_text = text
+
+        def write(self, text: str):
+            """ write text, ignoring newlines (does not add or remove \\n) """
+            self.target.write(text)
+            self.last_text = text
+
+        def __exit__(self, *args):
+            last_text_endswith_newline = self.last_text.rstrip(' \t\b').endswith('\n')
+            if not (self.last_source == self.source == 'c'):
+                if not last_text_endswith_newline:
+                    self.target.write('\n')
+                self.target.write(self.prev_console_output.rstrip())
+            if self.source == 'c' and last_text_endswith_newline:
+                if self.target_name == 'stderr':
+                    Console._console_stderr.clear()
+                else:
+                    Console._console_stdout.clear()
+            Console._last_source = self.source
+            Console._LOCK.release()
